01a8fd3beb01be6b9dd77a0a757b73c2ccf218ae
Paul Waring <paul@xk7.net>
2025-07-25 16:23:43 +0100

    Allow table names to be specified when exporting
    The export subcommand now supports a --table option, which can be
    passed zero or more times. Zero times means 'include all tables', and
    this is the default behaviour. If one or more tables are specified, only
    those tables will be included in the export.
    
    There is currently no error displayed if any of the table names do not
    exist.
    
    Closes #3

--- cmd/export.go
+++ cmd/export.go
@@ -66,6 +66,7 @@ var hostname string
 var port int
 var outputFile string
 var passwordPrompt bool
+var tableIncludes []string
 
 var exportCmd = &cobra.Command{
 	Use:   "export",
@@ -116,29 +117,31 @@ var exportCmd = &cobra.Command{
 		}
 
 		for t := range tables {
-			columnRows, err := db.Query(tableColumnsSql, database, tables[t].name)
-
-			if err != nil {
-				log.Fatal(err)
-			}
-
-			for columnRows.Next() {
-				column := DatabaseColumn{}
-
-				err := columnRows.Scan(
-					&column.column_name,
-					&column.column_default,
-					&column.is_nullable,
-					&column.data_type,
-					&column.column_type,
-					&column.column_key,
-				)
+			if inTableIncludes(tableIncludes, tables[t].name) {
+				columnRows, err := db.Query(tableColumnsSql, database, tables[t].name)
 
 				if err != nil {
 					log.Fatal(err)
 				}
 
-				tables[t].columns = append(tables[t].columns, column)
+				for columnRows.Next() {
+					column := DatabaseColumn{}
+
+					err := columnRows.Scan(
+						&column.column_name,
+						&column.column_default,
+						&column.is_nullable,
+						&column.data_type,
+						&column.column_type,
+						&column.column_key,
+					)
+
+					if err != nil {
+						log.Fatal(err)
+					}
+
+					tables[t].columns = append(tables[t].columns, column)
+				}
 			}
 		}
 
@@ -159,35 +162,37 @@ var exportCmd = &cobra.Command{
 		}
 
 		for t := range tables {
-			sheetName := tables[t].name
-			if len(sheetName) > maxSheetNameLength {
-				sheetName = tables[t].name[:maxSheetNameLength]
-			}
+			if len(tables[t].columns) >= 1 {
+				sheetName := tables[t].name
+				if len(sheetName) > maxSheetNameLength {
+					sheetName = tables[t].name[:maxSheetNameLength]
+				}
 
-			_, err := f.NewSheet(sheetName)
+				_, err := f.NewSheet(sheetName)
 
-			if err != nil {
-				log.Fatal(err)
-			}
+				if err != nil {
+					log.Fatal(err)
+				}
 
-			f.SetCellValue(sheetName, "A1", "Column Name")
-			f.SetCellValue(sheetName, "B1", "Column Default")
-			f.SetCellValue(sheetName, "C1", "Is Nullable")
-			f.SetCellValue(sheetName, "D1", "Data Type")
-			f.SetCellValue(sheetName, "E1", "Column Type")
-			f.SetCellValue(sheetName, "F1", "Column Key")
+				f.SetCellValue(sheetName, "A1", "Column Name")
+				f.SetCellValue(sheetName, "B1", "Column Default")
+				f.SetCellValue(sheetName, "C1", "Is Nullable")
+				f.SetCellValue(sheetName, "D1", "Data Type")
+				f.SetCellValue(sheetName, "E1", "Column Type")
+				f.SetCellValue(sheetName, "F1", "Column Key")
 
-			f.SetCellStyle(sheetName, "A1", "F1", boldStyle)
+				f.SetCellStyle(sheetName, "A1", "F1", boldStyle)
 
-			for c := range tables[t].columns {
-				row := strconv.Itoa(c + 2)
+				for c := range tables[t].columns {
+					row := strconv.Itoa(c + 2)
 
-				f.SetCellValue(sheetName, "A"+row, tables[t].columns[c].column_name)
-				f.SetCellValue(sheetName, "B"+row, tables[t].columns[c].column_default)
-				f.SetCellValue(sheetName, "C"+row, tables[t].columns[c].is_nullable)
-				f.SetCellValue(sheetName, "D"+row, tables[t].columns[c].data_type)
-				f.SetCellValue(sheetName, "E"+row, tables[t].columns[c].column_type)
-				f.SetCellValue(sheetName, "F"+row, tables[t].columns[c].column_key)
+					f.SetCellValue(sheetName, "A"+row, tables[t].columns[c].column_name)
+					f.SetCellValue(sheetName, "B"+row, tables[t].columns[c].column_default)
+					f.SetCellValue(sheetName, "C"+row, tables[t].columns[c].is_nullable)
+					f.SetCellValue(sheetName, "D"+row, tables[t].columns[c].data_type)
+					f.SetCellValue(sheetName, "E"+row, tables[t].columns[c].column_type)
+					f.SetCellValue(sheetName, "F"+row, tables[t].columns[c].column_key)
+				}
 			}
 		}
 
@@ -228,5 +233,21 @@ func init() {
 
 	exportCmd.Flags().BoolVarP(&passwordPrompt, "password", "p", false, "Prompt for password")
 
+	exportCmd.Flags().StringSliceVar(&tableIncludes, "table", []string{}, "Only include these tables")
+
 	rootCmd.AddCommand(exportCmd)
+}
+
+func inTableIncludes(tableIncludes []string, tableName string) bool {
+	if len(tableIncludes) == 0 {
+		return true
+	} else {
+		for ti := range tableIncludes {
+			if tableIncludes[ti] == tableName {
+				return true
+			}
+		}
+	}
+
+	return false
 }
